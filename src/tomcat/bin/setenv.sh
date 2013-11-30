d865c28af10b83f5ad6f769ddf5cf30be4542d1d
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2013-11-30 15:33:53 +0900

    fix #10

--- src/tomcat/bin/setenv.sh
+++ src/tomcat/bin/setenv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export JAVA_OPTS="$JAVA_OPTS -Dsolr.solr.home=$CATALINA_HOME/solr -Dfess.solr.data.dir=$CATALINA_HOME/solr/core1/data -Dfess.log.file=$CATALINA_HOME/webapps/fess/WEB-INF/logs/fess.out -Djava.awt.headless=true -server -Xmx1g -XX:MaxPermSize=128m -XX:-UseGCOverheadLimit -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing -XX:CMSIncrementalDutyCycleMin=0 -XX:+UseParNewGC -XX:+UseStringCache -XX:+UseTLAB -XX:+DisableExplicitGC"
+export JAVA_OPTS="$JAVA_OPTS -Dsolr.solr.home=$CATALINA_HOME/solr -Dfess.log.file=$CATALINA_HOME/webapps/fess/WEB-INF/logs/fess.out -Djava.awt.headless=true -server -Xmx1g -XX:MaxPermSize=128m -XX:-UseGCOverheadLimit -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing -XX:CMSIncrementalDutyCycleMin=0 -XX:+UseParNewGC -XX:+UseStringCache -XX:+UseTLAB -XX:+DisableExplicitGC"
 # -XX:+UseCompressedOops for 64bit OS
 # -XX:+UseCompressedStrings if java6u20 or above
 # -XX:+OptimizeStringConcat if java6u21 or above
