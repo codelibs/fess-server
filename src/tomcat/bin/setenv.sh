cd6b8f8a254d3a21075a20e15b8630189ba97419
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2014-03-08 22:35:04 +0900

    #35

--- src/tomcat/bin/setenv.sh
+++ src/tomcat/bin/setenv.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-export JAVA_OPTS="$JAVA_OPTS -Dsolr.solr.home=$CATALINA_HOME/solr -Dfess.log.file=$CATALINA_HOME/webapps/fess/WEB-INF/logs/fess.out -Dsolr.log.file=$CATALINA_HOME/logs/solr.log -Djava.awt.headless=true -server -Xmx1g -XX:MaxPermSize=128m -XX:-UseGCOverheadLimit -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing -XX:CMSIncrementalDutyCycleMin=0 -XX:+UseParNewGC -XX:+UseStringCache -XX:+UseTLAB -XX:+DisableExplicitGC"
-# -XX:+UseCompressedOops for 64bit OS
-# -XX:+UseCompressedStrings if java6u20 or above
-# -XX:+OptimizeStringConcat if java6u21 or above
+export JAVA_OPTS="$JAVA_OPTS -Dsolr.solr.home=$CATALINA_HOME/solr -Dfess.log.file=$CATALINA_HOME/webapps/fess/WEB-INF/logs/fess.out -Dsolr.log.file=$CATALINA_HOME/logs/solr.log -Djava.awt.headless=true -server -Xmx1g -XX:+UseTLAB -XX:+DisableExplicitGC"
 
+JAVA_VER=$(java -version 2>&1 | sed 's/.*version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
+if [ "$JAVA_VER" -ge 18 ] ; then
+  export JAVA_OPTS="$JAVA_OPTS -XX:MaxMetaspaceSize=128m -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=45 -XX:MaxGCPauseMillis=200"
+else
+  export JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=128m -XX:-UseGCOverheadLimit -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing -XX:CMSIncrementalDutyCycleMin=0 -XX:+UseParNewGC -XX:+UseStringCache -XX:+UseCompressedStrings -XX:+OptimizeStringConcat -XX:+UseCompressedOops"
+# if 32bit OS, remove -XX:+UseCompressedOops
 # if Java heap size is over 32GB heap, remove -XX:+UseCompressedOops
 # if your machine has 2 or more cpu, enabled -XX:+CMSParallelRemarkEnabled by -XX:+UseConcMarkSweepGC
+fi
 
 export CATALINA_OUT=/dev/null
